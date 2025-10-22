e1c915adad4287d37ac71d2e7b3de766eae2de4b
NeyTeKYT <florent.cagnard@gmail.com>
2025-10-22 11:19:40 +0200

    Ajout de l'onglet pour accéder au formulaire DHCP

---
+++ interface_web/control/formulaire_dhcp.php
@@ -0,0 +1,13 @@
+<?php
+
+	$racine_path = "../";	// Chemin vers la racine
+
+	include($racine_path . "templates/head.php");	// La balise <head> avec toutes les métadonnées 
+
+	include($racine_path . "templates/navbar.php");	// Barre de navigation pour pouvoir se déplacer entre les pages
+
+    include($racine_path . "templates/formulaire_dhcp.php");
+
+	include($racine_path . "templates/footer.php");
+	
+?>
---
+++ interface_web/templates/formulaire_dhcp.php
@@ -0,0 +1,3 @@
+<main>
+    <h1>Création du script en cours avant de déployer le formulaire</h1>
+</main>
